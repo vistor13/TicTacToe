81c1e4cb8402f69afd8d52ee071fc7980ab2dd2b
Віктор Поліщук <polishchuk.viktor1310@gmail.com>
2024-12-09 13:34:45 +0200

    feat: add method for 3x3 game board initialization

---
+++ backend/TicTacToe/TicTacToe.Core/Models/Board.cs
@@ -0,0 +1,24 @@
+﻿namespace TicTacToe.Core.Models
+{
+	public class Board
+	{
+		private const int BOARD_SIZE = 3;
+		public char[,] Grid {  get; set; }
+        public Board()
+        {
+            InitializeBoard();
+        }
+        private void InitializeBoard()
+		{
+			Grid = new char[BOARD_SIZE, BOARD_SIZE];
+			for (int i = 0; i < BOARD_SIZE; i++)
+			{ 
+			   for (int j = 0; j < BOARD_SIZE; j++)
+				{
+					Grid[i, j] = ' ';
+				}
+			}
+
+		}
+	}
+}
---
+++ backend/TicTacToe/TicTacToe.Core/Models/Game.cs
@@ -0,0 +1,32 @@
+﻿namespace TicTacToe.Core.Models
+{
+	public enum GameState
+	{
+		Ongoing,
+		Draw,
+		PlayerOneWin,
+		PlayerTwoWin
+	}
+
+	public enum PlayerTurn
+	{
+		X,
+		Y
+	}
+
+	public class Game
+	{
+		public Board GameBoard { get; set; }
+		public GameState State { get; set; }
+		public PlayerTurn CurrentTurn{ get; set; }
+		public Game() 
+		{
+			GameBoard= new Board();
+			State = GameState.Ongoing;
+			CurrentTurn = PlayerTurn.X;
+		}
+
+		
+	}
+	
+}
