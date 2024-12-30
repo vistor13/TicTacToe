8adda72e166721505c369aa87e2d0b8fc06bce27
Віктор Поліщук <polishchuk.viktor1310@gmail.com>
2024-12-30 22:01:52 +0200

    refactor: reorganize game logic.

--- backend/TicTacToe/TicTacToe.ConsoleUI/GameController.cs
+++ backend/TicTacToe/TicTacToe.ConsoleUI/GameController.cs
@@ -1,4 +1,5 @@
 using TicTacToe.Core.Commands;
+using TicTacToe.Core.CoreMessages;
 using TicTacToe.Core.Interfaces;
 using TicTacToe.Core.Models;
 
@@ -25,23 +26,77 @@ public class GameController(
                 continue;
             }
 
+            if (gameProcessor.GameModes == GameModes.GameWithAi)
+                PlayWithAi();
+            else if (gameProcessor.GameModes == GameModes.GameWithPlayer) PlayTwoPlayers();
+        }
+    }
+
+    private void PlayWithAi()
+    {
+        PlayGameLoop(
+            () =>
+            {
+                var aiMove = aiBot.FindBestMove();
+                var aiMoveCommand = new MoveCommand(gameProcessor, aiMove);
+                consoleRenderer.RenderMessage(
+                    string.Format(Messages.GameProcess.AiMove, aiMove.Row + 1, aiMove.Col + 1));
+                commandInvoker.Execute(aiMoveCommand);
+                var showAiCommand = new ShowBoardCommand(gameProcessor, consoleRenderer);
+                showAiCommand.Execute();
+            }
+        );
+    }
+
+    private void PlayTwoPlayers()
+    {
+        PlayGameLoop(() => { });
+    }
+
+    private void PlayGameLoop(Action aiMoveAction)
+    {
+        while (gameProcessor.GameModes != GameModes.NotDefined)
+        {
+            var command = GetCommand();
+            var executionResult = commandInvoker.Execute(command);
+
+            if (executionResult.IsError)
+            {
+                consoleRenderer.RenderError(executionResult.Errors.First().Description);
+                continue;
+            }
+
             if (command is MoveCommand)
             {
                 var showCommand = new ShowBoardCommand(gameProcessor, consoleRenderer);
                 showCommand.Execute();
-            }
 
-            if (gameProcessor.State is GameState.Win)
-            {
-                consoleRenderer.RenderWin(gameProcessor.CurrentTurn);
-                consoleRenderer.RenderProposeRestoreGame();
-            }
-            else if (gameProcessor.State is GameState.Draw)
-            {
-                consoleRenderer.RenderDraw();
-                consoleRenderer.RenderProposeRestoreGame();
+                if (HandleGameEndState()) break;
+
+                aiMoveAction.Invoke();
+
+                if (HandleGameEndState()) break;
             }
         }
+    }
+
+    private bool HandleGameEndState()
+    {
+        if (gameProcessor.State == GameState.Win)
+        {
+            consoleRenderer.RenderWin(gameProcessor.CurrentTurn);
+            consoleRenderer.RenderProposeRestoreGame();
+            return true;
+        }
+
+        if (gameProcessor.State == GameState.Draw)
+        {
+            consoleRenderer.RenderDraw();
+            consoleRenderer.RenderProposeRestoreGame();
+            return true;
+        }
+
+        return false;
     }
 
     private ICommand GetCommand()
