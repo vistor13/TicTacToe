fe691b93574a35f3504ddca91c995753489b137f
Віктор Поліщук <polishchuk.viktor1310@gmail.com>
2025-01-17 11:28:00 +0200

    feat: register GameService as a singleton in dependency injection

--- backend/TicTacToe/TicTacToe.Api/Extensions/DependencyInjectionExtension.cs
+++ backend/TicTacToe/TicTacToe.Api/Extensions/DependencyInjectionExtension.cs
@@ -10,5 +10,6 @@ public static class DependencyInjectionExtension
         services.AddScoped<IGameProcessor, GameProcessor>();
         services.AddScoped<ICommandInvoker, CommandInvoker>();
         services.AddScoped<IMiniMaxAi, MiniMaxAi>();
+        services.AddSingleton<GameService>();
     }
 }
---
+++ backend/TicTacToe/TicTacToe.Core/Services/GameService.cs
@@ -0,0 +1,18 @@
+using TicTacToe.Core.Dto;
+
+namespace TicTacToe.Core.Services;
+
+public class GameService
+{
+    private readonly Dictionary<Guid, GameStateDto> _games = new();
+
+    public void SaveGame(Guid gameId, GameStateDto gameState)
+    {
+        _games[gameId] = gameState;
+    }
+
+    public GameStateDto? GetGame(Guid gameId)
+    {
+        return _games.GetValueOrDefault(gameId);
+    }
+}
