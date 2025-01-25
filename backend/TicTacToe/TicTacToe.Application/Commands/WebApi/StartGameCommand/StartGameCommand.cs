a15605946230342db2cd25209d59ca18c4d49922
Віктор Поліщук <polishchuk.viktor1310@gmail.com>
2025-01-25 11:41:47 +0200

    refactor: replace class with record in StartGameCommand

--- backend/TicTacToe/TicTacToe.Application/Commands/WebApi/StartGameCommand/StartGameCommand.cs
+++ backend/TicTacToe/TicTacToe.Application/Commands/WebApi/StartGameCommand/StartGameCommand.cs
@@ -3,7 +3,4 @@ using TicTacToe.Application.Dto;
 
 namespace TicTacToe.Application.Commands.WebApi.StartGameCommand;
 
-public class StartGameCommand(bool isTwoPlayerMode) : IRequest<GameInitializationDto>
-{
-    public bool IsTwoPlayerMode { get; set; } = isTwoPlayerMode;
-}
+public sealed record StartGameCommand(bool IsTwoPlayerMode) : IRequest<GameInitializationDto>;
