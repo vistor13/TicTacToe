08a871fcb0ac880eb7414e7bab5ac06214907d8a
Віктор Поліщук <polishchuk.viktor1310@gmail.com>
2025-02-19 10:18:47 +0200

    feat : update classes

--- backend/TicTacToe/TicTacToe.Infrastructure/Interfaces/IRepositoryBase.cs
+++ backend/TicTacToe/TicTacToe.Infrastructure/Interfaces/IRepositoryBase.cs
@@ -1,4 +1,3 @@
-using TicTacToe.Infrastructure.DataBase.Specifications;
 using TicTacToe.Infrastructure.Entities;
 
 namespace TicTacToe.Infrastructure.Interfaces;
@@ -8,6 +7,5 @@ public interface IRepositoriesBase<TEntity> where TEntity : BaseEntity
     Task<TEntity?> GetById(long id);
     Task Delete(long id);
     Task<TEntity> Create(TEntity entity);
-    Task<TEntity> GetFirstBySpecification(Specification<TEntity> specification);
     Task Update(long id, TEntity entity);
 }
