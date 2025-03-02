1f5883d94fb1164762c9d587eaa4486ce98cbf7c
Віктор Поліщук <polishchuk.viktor1310@gmail.com>
2025-03-02 16:06:30 +0200

    feat: modify routes

--- frontend/TicTacToe/src/app/app.routes.ts
+++ frontend/TicTacToe/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {GameOptionsComponent} from './pages/game-options/game-options.component';
+import {GameComponent} from './pages/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes =
+[
+  {path : '', component : GameOptionsComponent },
+  {path : 'game', component : GameComponent},
+];
