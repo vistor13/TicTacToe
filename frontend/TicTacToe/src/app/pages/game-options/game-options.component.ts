0f81c7e15c35fea9e070e55e042e33ca1ac45775
Віктор Поліщук <polishchuk.viktor1310@gmail.com>
2025-03-02 16:06:08 +0200

    feat: add new pages

---
+++ frontend/TicTacToe/src/app/pages/game-options/game-options.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from '../../common-ui/header/header.component';
+
+@Component({
+  selector: 'app-game-options',
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './game-options.component.html',
+  styleUrl: './game-options.component.scss'
+})
+export class GameOptionsComponent {
+
+}
---
+++ frontend/TicTacToe/src/app/pages/game/game.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from '../../common-ui/header/header.component';
+
+@Component({
+  selector: 'app-game',
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent {
+
+}
