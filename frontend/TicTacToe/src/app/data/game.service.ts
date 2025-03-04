d759ecb502541b237eb2b8264fecef8fb67e8b0b
Віктор Поліщук <polishchuk.viktor1310@gmail.com>
2025-03-04 13:54:37 +0200

    feat : add main logic game.

--- frontend/TicTacToe/src/app/data/game.service.ts
+++ frontend/TicTacToe/src/app/data/game.service.ts
@@ -1,5 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {GameStateResponse, StartGameResponse} from '../inerfaces/game.interface';
+import {tap} from 'rxjs';
+import {CookieService} from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +10,37 @@ import {HttpClient} from '@angular/common/http';
 export class GameService {
 
   http : HttpClient = inject(HttpClient);
+  private cookieService = inject(CookieService);
+  private apiUrl = 'http://localhost:5120/api/game';
 
-  private apiUrl = 'http://localhost:5120/api/game/start';
   startGame(isTwoPlayerMode: boolean) {
-    return this.http.post<void>(this.apiUrl, null, {
+    return this.http.post<StartGameResponse>(`${this.apiUrl}/start`, null, {
       params: { isTwoPlayerMode: isTwoPlayerMode.toString() },
+    }).pipe(
+      tap(res => {
+        this.cookieService.set("gameId", res.id.toString());
+      })
+    );
+  }
+
+  getGame() {
+    const gameId = this.getGameId();
+    return this.http.get<GameStateResponse>(`${this.apiUrl}/state`, {
+      params: { gameId: gameId.toString() }
     });
   }
+
+  getGameId(): number {
+    return Number(this.cookieService.get("gameId"));
+  }
+
+  makeMove(row: number, col: number) {
+    const gameId = this.getGameId();
+    return this.http.post<GameStateResponse>(`${this.apiUrl}/move`, {
+      gameId: gameId,
+      row: row,
+      col: col
+    });
+  }
+
 }
--- frontend/TicTacToe/src/app/pages/game/game.component.ts
+++ frontend/TicTacToe/src/app/pages/game/game.component.ts
@@ -1,49 +1,36 @@
-import { Component } from '@angular/core';
-import {HeaderComponent} from '../../common-ui/header/header.component';
+import { Component, inject } from '@angular/core';
+import { GameService } from '../../data/game.service';
+import {GameStateResponse} from '../../inerfaces/game.interface';
 import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-game',
-  imports: [
-    HeaderComponent,
-    NgForOf,
-    NgIf
-  ],
+  standalone: true,
   templateUrl: './game.component.html',
+  imports: [NgForOf, NgIf],
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-  board: string[] = Array(9).fill('');
-  currentPlayer: 'X' | 'O' = 'X';
-  winner: string | null = null;
+  gameService: GameService = inject(GameService);
+  gameState!: GameStateResponse;
 
-  makeMove(index: number) {
-    if (!this.board[index] && !this.winner) {
-      this.board[index] = this.currentPlayer;
-      if (this.checkWinner()) {
-        this.winner = this.currentPlayer;
-      } else {
-        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+  constructor() {
+    this.loadGameState();
+  }
+
+  loadGameState() {
+    this.gameService.getGame().subscribe(
+      (response) => {
+        this.gameState = response;
       }
-    }
+    );
   }
 
-  checkWinner(): boolean {
-    const winPatterns = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Горизонтальні лінії
-      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Вертикальні лінії
-      [0, 4, 8], [2, 4, 6]  // Діагоналі
-    ];
-
-    return winPatterns.some(pattern => {
-      const [a, b, c] = pattern;
-      return this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c];
-    });
-  }
-
-  resetGame() {
-    this.board = Array(9).fill('');
-    this.currentPlayer = 'X';
-    this.winner = null;
+  makeMove(row: number, col: number) {
+    this.gameService.makeMove(row, col)
+      .subscribe(() => {
+        this.loadGameState();
+      }
+    );
   }
 }
