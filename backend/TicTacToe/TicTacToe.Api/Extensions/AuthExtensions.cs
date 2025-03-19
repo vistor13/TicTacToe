ff69f4c7b5427053c5e8de6c39c13cf853511d6c
Віктор Поліщук <polishchuk.viktor1310@gmail.com>
2025-03-19 09:43:16 +0200

    refactor: relocate managmentApi DI

--- backend/TicTacToe/TicTacToe.Api/Extensions/AuthExtensions.cs
+++ backend/TicTacToe/TicTacToe.Api/Extensions/AuthExtensions.cs
@@ -1,6 +1,11 @@
 using System.Security.Claims;
+using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
+using Auth0.ManagementApi;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using TicTacToe.Infrastructure.Auth;
 
 namespace TicTacToe.Api.Extensions;
 
@@ -28,6 +33,31 @@ public static class AuthExtensions
                     ValidIssuer = configuration["Auth0:Domain"]
                 };
             });
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuth0ManagementApiClient(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSingleton<IManagementApiClient>(provider =>
+        {
+            var config = provider.GetRequiredService<IOptions<Auth0Options>>().Value;
+
+            var auth0AuthClient = new AuthenticationApiClient(new Uri($"https://{config.Domain}/"));
+
+            var tokenResponse = auth0AuthClient.GetTokenAsync(new ClientCredentialsTokenRequest
+            {
+                ClientId = config.ClientId,
+                ClientSecret = config.ClientSecret,
+                Audience = $"https://{config.Domain}/api/v2/"
+            }).Result;
+
+            var auth0Client =
+                new ManagementApiClient(tokenResponse.AccessToken, new Uri($"https://{config.Domain}/api/v2/"));
+
+            return auth0Client;
+        });
 
         return services;
     }
--- backend/TicTacToe/TicTacToe.Api/Extensions/DependencyInjectionExtension.cs
+++ backend/TicTacToe/TicTacToe.Api/Extensions/DependencyInjectionExtension.cs
@@ -1,9 +1,4 @@
-using Auth0.AuthenticationApi;
-using Auth0.AuthenticationApi.Models;
-using Auth0.ManagementApi;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using TicTacToe.Infrastructure.Auth;
 using TicTacToe.Infrastructure.DataBase;
 
 namespace TicTacToe.Api.Extensions;
@@ -25,31 +20,6 @@ public static class DependencyInjectionExtension
             opt.UseNpgsql(configuration["Database:ConnectionString"],
                 b => b.MigrationsAssembly("TicTacToe.Infrastructure"));
         });
-        return services;
-    }
-
-    public static IServiceCollection AddAuth0ManagementApiClient(this IServiceCollection services,
-        IConfiguration configuration)
-    {
-        services.AddSingleton<IManagementApiClient>(provider =>
-        {
-            var config = provider.GetRequiredService<IOptions<Auth0Options>>().Value;
-
-            var auth0AuthClient = new AuthenticationApiClient(new Uri($"https://{config.Domain}/"));
-
-            var tokenResponse = auth0AuthClient.GetTokenAsync(new ClientCredentialsTokenRequest
-            {
-                ClientId = config.ClientId,
-                ClientSecret = config.ClientSecret,
-                Audience = $"https://{config.Domain}/api/v2/"
-            }).Result;
-
-            var auth0Client =
-                new ManagementApiClient(tokenResponse.AccessToken, new Uri($"https://{config.Domain}/api/v2/"));
-
-            return auth0Client;
-        });
-
         return services;
     }
 }
